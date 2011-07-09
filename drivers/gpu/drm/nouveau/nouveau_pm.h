c7d6165177ae058479104e5aceddea93ac0d4088
Martin Peres <martin.peres@ensi-bourges.fr>
2011-07-10 01:31:25 +0200

    drm/nv50/pm: use the new hooks and pause before reclocking
    Signed-off-by: Martin Peres <martin.peres@ensi-bourges.fr>

--- drivers/gpu/drm/nouveau/nouveau_pm.h
+++ drivers/gpu/drm/nouveau/nouveau_pm.h
@@ -62,10 +62,9 @@ int nv41_pm_fanspeed_get(struct drm_device *);
 int nv41_pm_fanspeed_set(struct drm_device *, int percent);
 
 /* nv50_pm.c */
-int nv50_pm_clock_get(struct drm_device *, u32 id);
-void *nv50_pm_clock_pre(struct drm_device *, struct nouveau_pm_level *,
-			u32 id, int khz);
-void nv50_pm_clock_set(struct drm_device *, void *);
+int nv50_pm_clocks_get(struct drm_device *, struct nouveau_pm_level *);
+void *nv50_pm_clocks_pre(struct drm_device *, struct nouveau_pm_level *);
+int nv50_pm_clocks_set(struct drm_device *, void *);
 int nv50_pm_fanspeed_get(struct drm_device *);
 int nv50_pm_fanspeed_set(struct drm_device *, int percent);
 
